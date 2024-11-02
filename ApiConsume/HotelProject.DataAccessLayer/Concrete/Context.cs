e32f9886f6516fa84df4ef460b7b4c414a672bf0
Hakan-Hakyemez <khan.notunfair@gmail.com>
2024-11-02 14:28:32 +0300

    ...

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -13,7 +13,7 @@ namespace HotelProject.DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=BUNYAMINPILSIM\\SQLEXPRESS;initial catalog=HotelApiDb;integrated security=true;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-3MI4F2H;initial catalog=HotelApiDb;integrated security=true;TrustServerCertificate=true");
         }
 
         public DbSet<Room> Rooms { get; set; }
